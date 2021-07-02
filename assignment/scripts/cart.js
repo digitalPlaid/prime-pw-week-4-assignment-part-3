3a44a990f2dc2de8f43fa05142e25f3ec2da64d9
Dan St. Aubin <staubindj@gmail.com>
2021-07-02 09:01:58 -0500

    working on stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,72 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem( str ) {
+    if (!isFull()) {
+        basket.push(str);
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
+
+
+// Tests
+let str = 'blueberries';
+console.log(`Did we push ${str} to basket? ${addItem(str)}`)
+console.log(`Double check: ${basket}`)
+// what if str is blank string?
+str = ''
+// should contain the 'blueberries' from before and a blank string
+console.log(`Did we push ${str} to basket? ${addItem(str)}`)
+console.log(`Double check: ${basket}`) // note the comma, delimiting a second value which appears blank because it's the
+// empty string
+// direct inspection of the basket variable after running this, which shows the brackets and quotes, confirms it's there.
+// maybe we wouldn't want to be able to add an empty string, but I'm going to allow it for now. We could simply
+// filter it out with something like if (!str) return false;
+
+function listItems() {
+    for (let element of basket) {
+        console.log(element)
+    }
+}
+// tests
+copyBasket = basket.slice()
+basket = [1,2,3,4,5]
+listItems()
+basket = copyBasket.slice()
+
+// something about how I wrote this feels like it's not good practice. Hm.
+function empty() {
+    basket = []
+}
+// test that the basket is in fact empty:
+empty()
+listItems() // basket is empty, as we had hoped.
+
+//           STRETCH GOALS          //
+
+const maxItems = 5;
+
+function isFull() {
+    if (basket.length < maxItems) return false;
+    return true;
+}
+/*
+Update the cart.js file to do the following:
+
+
+
+
+Create a function called removeItem. It should:
+Take an input parameter for a string item
+Use Array.indexOf to find the index of the first matching item in the basket.
+Use Array.splice to remove the first matching item from the basket.
+Return the item removed or null if the item was not found
+*/
